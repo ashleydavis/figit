ecaaf43f1f57786ac5d390cf95c4627994834309
Ashley Davis <ashley@codecapers.com.au>
2021-03-10 17:06:55 +1000

    Added better help when no files are speciifed.

--- src/cli.ts
+++ src/cli.ts
@@ -5,6 +5,14 @@ import * as minimist from "minimist";
 async function main(): Promise<void> {
     const argv = minimist(process.argv.slice(2));
     const filePaths = argv._.map(filePath => path.resolve(__dirname, "..", filePath));
+    if (filePaths.length === 0) {
+        console.error(`No files specified.`);
+        console.log(`Usage: figit [files...] --output=json|yaml`)
+        console.log(`Default output: json`);
+        console.log(`Example: figit myconfig.js --output=yaml`);
+        process.exit(1);
+    }
+
     for await (const output of figit({ filePaths: filePaths, output: argv.output })) {
         console.log(output);
     }
@@ -14,4 +22,5 @@ main()
     .catch(err => {
         console.error(`An error occurred.`);
         console.error(err.message || err.stack || err);
+        process.exit(1);
     });
